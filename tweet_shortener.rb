6b2e483cf6192e4ae71759c29bd6a6c39f0aad8c
Samidh Patel <spatel13456@gmail.com>
2019-01-16 18:20:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,15 +14,7 @@ def dictionary
 end
 
 def word_substituter(tweets)
-  tweet = tweets.split(" ")
-  tweet.each_with_index do |words, index|
-    dictionary.keys.each do |a|
-      if a == words 
-        tweet[index] = dictionary[a]
-      end
-    end
-  end
-  tweet.join(" ")    
+  
 end
 
 def bulk_tweet_shortener(array)
