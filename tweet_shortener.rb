6f8baa0846da009ecc36e76d987923589822c46e
Samidh Patel <spatel13456@gmail.com>
2019-01-16 18:24:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,7 +14,7 @@ def dictionary
 end
 
 def word_substituter(tweets)
-  tweet.split(" ").map do |word|
+  tweets.split(" ").map do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
